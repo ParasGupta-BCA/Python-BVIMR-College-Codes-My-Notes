d642f30d39cc81e36769c6891593e250340d509b
Paras Gupta <88033881+ParasGupta-BCA@users.noreply.github.com>
2025-09-02 12:10:46 +0530

    Update chain_generators.py

--- chain_generators.py
+++ chain_generators.py
@@ -9,3 +9,6 @@ def count_up_to(max):
 def squared_numbers(numbers):
   for number in number:
     yield number**2
+
+for square in squared_number(count_up_to(5)):
+  print(square) #Output: 1,4,,9,16,25
